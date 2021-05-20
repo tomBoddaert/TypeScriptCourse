eca586fc79016a338933a4b248489dd47bfdd1e1
Tom Boddaert <tboddaert@outlook.com>
2021-05-20 16:31:34 +0100

    NPM and TSC update

---
+++ 1_-_Intro/2_-_Node_js.ts
@@ -0,0 +1,11 @@
+/*
+
+Node.js is a JavaScript interpreter based on the V8 JavaScript engine
+It is currently the most popular server-side JS interpreter
+
+To run a JavaScript file with node, we use 'node /path/to/file.js' in the
+  command-line, you may need to navigate to the folder first
+To change "directory" in the command-line, use 'cd x', where x is the
+  subdirectory
+
+*/
---
+++ 1_-_Intro/3_-_npm_and_tsc.ts
@@ -0,0 +1,17 @@
+/*
+
+The Node Package Manager (npm) allows us to install packages and modules
+  for use in Node.js
+It can also install related JavaScript programs (Node Package eXecutables)
+
+We can use 'npm install x' (or 'npm i x') to install a package called 'x'
+Then, executables in the package can be called with 'npx x', where 'x' is
+  the command; these usually take command-line arguments
+
+The TypeScript Compiler (tsc) transpiles TypeScript files ('.ts'
+  extension) to JavaScript ('.js' extension), which can be run with node
+To install the TypeScript package, we use 'npm i typescript'
+To run tsc, we use 'npx tsc path/to/file.ts', which generates a JavaScript
+  file at 'path/to/file.js'
+
+*/
