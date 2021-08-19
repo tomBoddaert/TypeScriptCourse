e05281f0d08aa5d744ea02fe188eac977dce04e1
tomBoddaert <70723965+tomBoddaert@users.noreply.github.com>
2021-08-19 17:42:00 +0100

    Delete Old directory
    Removal of v1 from v2

--- Old/2_-_Variables_and_log/1_-_log.ts
+++
@@ -1,9 +0,0 @@
-/*
-https://developer.mozilla.org/en-US/docs/Web/API/Console/log
-
-Output text to the terminal using console.log()
-
-*/
-
-console.log('Hello world!');
-// Hello world!
--- Old/7_-_in_browser/testScript1.ts
+++
@@ -1,3 +0,0 @@
-var i = 0;
-
-setInterval(() => console.log(`This is message #${i++}`), 2000);
